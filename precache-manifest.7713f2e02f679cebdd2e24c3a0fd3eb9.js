ee92fc84729d7f4c869a29e7f94a95226e83c198
irynailina <irynailina@gmail.com>
2020-05-03 23:52:06 +0300

    Updates

---
+++ precache-manifest.7713f2e02f679cebdd2e24c3a0fd3eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd50ac0e514f9ea437a675f364d24ba2",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "be8bf801796327722697",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.f915d005.chunk.css"
+  },
+  {
+    "revision": "45bc48848416a393f3b0",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.456e528e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.456e528e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8bf801796327722697",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.d350e4be.chunk.js"
+  },
+  {
+    "revision": "c73043600cf8c264972f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2d9dc3c9.js"
+  }
+]);
